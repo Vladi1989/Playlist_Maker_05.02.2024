2017962dd0ec33b944403ac6376fb8205016c00a
Vladi1989 <bboyvladi1989@mail.ru>
2024-02-05 11:49:09 +0300

    Initial commit

---
+++ app/src/main/java/com/spase_y/playlistmaker05022024/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.spase_y.playlistmaker05022024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
