742e803ec87f2516bba318bf901f0e881e611e1c
Vladi1989 <bboyvladi1989@mail.ru>
2024-02-16 16:16:44 +0300

    finish task 9 sprint

--- app/src/main/java/com/spase_y/playlistmaker05022024/SettingsActivity.kt
+++ app/src/main/java/com/spase_y/playlistmaker05022024/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.spase_y.playlistmaker05022024
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,31 @@ class SettingsActivity : AppCompatActivity() {
 
         val backFromSetting = findViewById<ImageButton>(R.id.buttonBack)
         backFromSetting.setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+            val myIntent = Intent(this,MainActivity::class.java)
+            startActivity(myIntent)
+        }
+        val btnShare = findViewById<ImageView>(R.id.btnShare)
+        btnShare.setOnClickListener {
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type="text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.shared_text));
+            startActivity(Intent.createChooser(shareIntent,getString(R.string.shared_text)))
+        }
+
+        val btnSupport = findViewById<ImageView>(R.id.btnSupport)
+        btnSupport.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_mail)))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.my_app_share_text))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.my_app_share_theme))
+            startActivity(Intent.createChooser(shareIntent,getString(R.string.my_app_share_theme)))
+        }
+        val btnAgreement = findViewById<ImageView>(R.id.btnUserAgreement)
+        btnAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.practicun_offer)))
+            startActivity(intent)
         }
     }
 }
